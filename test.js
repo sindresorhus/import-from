f48813d43aff3cb6b72b38120ea29ffa2a15b1cc
Ran Yitzhaki <rany@cocycles.com>
2017-04-25 15:09:24 +0300

    add tests for error message and code

--- test.js
+++ test.js
@@ -4,6 +4,11 @@ import fn from './';
 test(t => {
 	t.is(fn('fixture', './fixture'), 'unicorn');
 	t.throws(() => fn('fixture', './nonexistent'));
+	const moduleNotFoundError = t.throws(() => {
+		fn('fixture', './nonexistent');
+	}, Error);
+	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
+	t.is(moduleNotFoundError.message, 'Cannot find module \'./nonexistent\'');
 	t.is(fn.silent('fixture', './fixture'), 'unicorn');
 	t.is(fn.silent('fixture', './nonexistent'), null);
 });
