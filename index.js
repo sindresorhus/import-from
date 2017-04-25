ae605ccb2934d0b0e621fbec2e6ffd7a4c1d930b
Ran Yitzhaki <rany@cocycles.com>
2017-04-25 15:08:54 +0300

    throw a module not found error instead of assertion error

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 'use strict';
-var resolveFrom = require('resolve-from');
+const resolveFrom = require('resolve-from');
+
+function createModuleNotFoundErr(path) {
+	const err = new Error('Cannot find module \'' + path + '\'');
+	err.code = 'MODULE_NOT_FOUND';
+	return err;
+}
 
 module.exports = function (fromDir, moduleId) {
-	return require(resolveFrom(fromDir, moduleId));
+	try {
+		return require(resolveFrom(fromDir, moduleId));
+	} catch (err) {
+		if (err.message === 'missing path') {
+			throw createModuleNotFoundErr(moduleId);
+		}
+	}
 };
 
 module.exports.silent = function (fromDir, moduleId) {
