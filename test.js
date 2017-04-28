fd681d468a27db69830f5a3fbc510a164f320be8
Ran Yitzhaki <rany@cocycles.com>
2017-04-28 11:33:08 +0300

    seperate reqFrom and reqFrom.silent tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import fn from './';
 
-test(t => {
+test('reqFrom', t => {
 	t.is(fn('fixture', './fixture'), 'unicorn');
 	t.throws(() => fn('fixture', './nonexistent'));
 	const moduleNotFoundError = t.throws(() => {
@@ -9,6 +9,11 @@ test(t => {
 	}, Error);
 	t.is(moduleNotFoundError.code, 'MODULE_NOT_FOUND');
 	t.is(moduleNotFoundError.message, 'Cannot find module \'./nonexistent\'');
+	t.is(fn.silent('fixture', './fixture'), 'unicorn');
+	t.is(fn.silent('fixture', './nonexistent'), null);
+});
+
+test('reqFrom,silent', t => {
 	t.is(fn.silent('fixture', './fixture'), 'unicorn');
 	t.is(fn.silent('fixture', './nonexistent'), null);
 });
